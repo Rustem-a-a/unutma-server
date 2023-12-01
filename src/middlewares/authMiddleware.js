af65bd73b24ec8ead6501ab236e0078b84b681be
RustemAbdulaev <rustem.abdulaev@gmail.com>
2023-12-01 22:23:05 +0300

    feat: tokenService,authMiddleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+import ApiError from "../exceptions/apiError.js";
+import tokenService from "../services/tokenService.js";
+
+export default function (req, res,next) {
+    try{
+    const authorizationHeaders = req.headers.authorization;
+    if(!authorizationHeaders){
+        return next(ApiError.Unauthorized());
+    }
+    const accessToken = authorizationHeaders.split(' ')[1]
+    if(!accessToken){
+        return next(ApiError.Unauthorized());
+    }
+    const userData = tokenService.validateAccessToken(accessToken);
+    if(!userData){
+        return next(ApiError.Unauthorized());
+        }
+    req.body.user = userData;
+    next();
+    }catch (e) {
+       return  ApiError.Unauthorized()
+    }
+}
---
+++ src/services/tokenService.js
@@ -0,0 +1,52 @@
+import jwt from "jsonwebtoken";
+import Token from "../models/tokenModel.js";
+
+class TokenService {
+    generateToken(payload) {
+        const accessToken = jwt.sign(payload, process.env.ACCESS_SECRET, {expiresIn: '60d'});
+        const refreshToken = jwt.sign(payload, process.env.REFRESH_SECRET, {expiresIn: '60d'});
+        return {
+            accessToken,
+            refreshToken
+        }
+    }
+
+    async saveToken(userId, refreshToken) {
+        const tokenData = await Token.findOne({user: userId});
+        if (tokenData) {
+            tokenData.refreshToken = refreshToken;
+            return await tokenData.save();
+        }
+        return await Token.create({user: userId, refreshToken});
+    }
+
+    async removeToken(refreshToken) {
+        const removedToken = await Token.deleteOne({refreshToken});
+        return removedToken;
+    }
+
+     validateAccessToken(accessToken) {
+        try {
+            const userData = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(refreshToken) {
+        try {
+            const userData = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await Token.findOne({refreshToken});
+        return tokenData;
+    }
+}
+
+export default new TokenService();
