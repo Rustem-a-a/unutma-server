350bad93ee0b7acbde7521b43ec0bb691a56501c
RustemAbdulaev <rustem.abdulaev@gmail.com>
2023-12-07 07:46:55 +0300

    fix: auth validation

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -8,7 +8,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 const router = new Router()
 
 router.post('/registration',
-    body('username').isLength({min: 5, max: 20}),
+    body('username').isLength({min: 3, max: 20}),
     body('email').isEmail(),
     body('password').isLength({min: 5, max: 20}),
     authController.registration)
